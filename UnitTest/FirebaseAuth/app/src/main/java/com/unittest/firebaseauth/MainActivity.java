e7e55315aaef6b5265f2f21e33eedb163fbb9557
YeonJun Jo <micayel1219@gmail.com>
2022-01-13 15:08:39 +0900

    commit 4: firebase authorization pj add

---
+++ UnitTest/FirebaseAuth/app/src/main/java/com/unittest/firebaseauth/MainActivity.java
@@ -0,0 +1,57 @@
+package com.unittest.firebaseauth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private Button register_btn, login_btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        register_btn = findViewById(R.id.register_btn);
+        login_btn = findViewById(R.id.login_btn);
+
+        setOnClick();
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    private void setOnClick(){
+        register_btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, RegisterActivity.class));
+            }
+        });
+
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            }
+        });
+    }
+
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+
+        }
+    }
+}
