95ed2f44587835440d668c2009cca006785ba042
YeonJun Jo <micayel1219@gmail.com>
2022-01-16 14:12:34 +0900

    7th commit : reset pwd on email add

--- UnitTest/FirebaseAuth/app/src/main/java/com/unittest/firebaseauth/MainActivity.java
+++ UnitTest/FirebaseAuth/app/src/main/java/com/unittest/firebaseauth/MainActivity.java
@@ -8,12 +8,11 @@ import android.view.View;
 import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
-    private Button register_btn, login_btn;
+    private Button register_btn, login_btn, send_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
 
         register_btn = findViewById(R.id.register_btn);
         login_btn = findViewById(R.id.login_btn);
+        send_btn = findViewById(R.id.send_btn);
 
         setOnClick();
         mAuth = FirebaseAuth.getInstance();
@@ -41,17 +41,24 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, LoginActivity.class));
             }
         });
+
+        send_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, ResetPwdActivity.class));
+            }
+        });
     }
 
 
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        if(currentUser != null){
-
-        }
-    }
+//    @Override
+//    public void onStart() {
+//        super.onStart();
+//        // Check if user is signed in (non-null) and update UI accordingly.
+//        FirebaseUser currentUser = mAuth.getCurrentUser();
+//        if(currentUser != null){
+//
+//        }
+//    }
 }
---
+++ UnitTest/FirebaseAuth/app/src/main/java/com/unittest/firebaseauth/ResetPwdActivity.java
@@ -0,0 +1,76 @@
+package com.unittest.firebaseauth;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class ResetPwdActivity extends AppCompatActivity {
+
+
+    private Button send_btn;
+    private EditText email_editText;
+    private FirebaseAuth mAuth;
+    private String TAG = "ResetPwdActivity";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reset_pwd);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        send_btn = findViewById(R.id.send_btn);
+        email_editText = findViewById(R.id.editTextTextEmailAddress);
+
+        send_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String emailAddress = email_editText.getText().toString();
+                send(emailAddress);
+            }
+        });
+
+
+
+    }
+
+    private void send(String addr){
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+
+        auth.sendPasswordResetEmail(addr)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "Email sent.");
+                            Toast.makeText(ResetPwdActivity.this, "Email sent", Toast.LENGTH_SHORT).show();
+//                            openEmailApp();
+                        }
+                    }
+                });
+    }
+
+    private void openEmailApp(){
+
+        try {
+            Intent intent = new Intent(Intent.ACTION_MAIN);
+            intent.addCategory(Intent.CATEGORY_APP_EMAIL);
+            startActivity(intent);
+        } catch (android.content.ActivityNotFoundException e) {
+            Toast.makeText(ResetPwdActivity.this, "There is no email client installed.", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
