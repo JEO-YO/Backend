35aa26163c94011b916e7722655f375e2917d973
YeonJun Jo <micayel1219@gmail.com>
2022-01-19 17:15:31 +0900

    9th commit : upload post pj add

---
+++ UnitTest/FirebasePost/app/src/main/java/com/unittest/firebasepost/Model/Post.java
@@ -0,0 +1,107 @@
+package com.unittest.firebasepost.Model;
+
+
+public class Post {
+    private String key;
+    private String classify;
+    private String category;
+    private String period;
+    private String frequency;
+    private String memberCnt;
+    private String memberCondition;
+    private String title;
+    private String description;
+    private String thumbnail = "";
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public Post(String key, String classify, String category, String period, String frequency, String memberCnt, String memberCondition, String title, String description) {
+        this.key = key;
+        this.classify = classify;
+        this.category = category;
+        this.period = period;
+        this.frequency = frequency;
+        this.memberCnt = memberCnt;
+        this.memberCondition = memberCondition;
+        this.title = title;
+        this.description = description;
+    }
+
+    public String getClassify() {
+        return classify;
+    }
+
+    public void setClassify(String classify) {
+        this.classify = classify;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getPeriod() {
+        return period;
+    }
+
+    public void setPeriod(String period) {
+        this.period = period;
+    }
+
+    public String getFrequency() {
+        return frequency;
+    }
+
+    public void setFrequency(String frequency) {
+        this.frequency = frequency;
+    }
+
+    public String getMemberCnt() {
+        return memberCnt;
+    }
+
+    public void setMemberCnt(String memberCnt) {
+        this.memberCnt = memberCnt;
+    }
+
+    public String getMemberCondition() {
+        return memberCondition;
+    }
+
+    public void setMemberCondition(String memberCondition) {
+        this.memberCondition = memberCondition;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getThumbnail() {
+        return thumbnail;
+    }
+
+    public void setThumbnail(String thumbnail) {
+        this.thumbnail = thumbnail;
+    }
+}
---
+++ UnitTest/FirebasePost/app/src/main/java/com/unittest/firebasepost/UploadActivity.java
@@ -0,0 +1,137 @@
+package com.unittest.firebasepost;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.unittest.firebasepost.Model.Post;
+
+import java.io.ByteArrayOutputStream;
+import java.util.HashMap;
+
+public class UploadActivity extends AppCompatActivity {
+
+    private ImageView imageView_thumbnail;
+
+    private EditText editText_classify, editText_category , editText_period
+            , editText_frequency, editText_memberCnt, editText_memberCdt, editText_title,editText_description;
+    private Button button_pick, button_upload;
+    private String classify, category, period, frequency, memberCnt, memberCdt, title, description;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload);
+
+        SetId();
+        setOnclick();
+    }
+
+    private void SetId(){
+        imageView_thumbnail = findViewById(R.id.imageView_thumbnail);
+
+        editText_classify = findViewById(R.id.editTextClassify);
+        editText_category = findViewById(R.id.editTextCategory);
+        editText_period = findViewById(R.id.editTextPeriod);
+        editText_frequency = findViewById(R.id.editTextFrequency);
+        editText_memberCnt = findViewById(R.id.editTextMemberCnt);
+        editText_memberCdt = findViewById(R.id.editTextMemberCdt);
+        editText_title = findViewById(R.id.editTextTitle);
+        editText_description = findViewById(R.id.editTextDescription);
+
+        button_pick = findViewById(R.id.pick_btn);
+        button_upload = findViewById(R.id.upload_btn);
+
+    }
+
+    private String setString( EditText editText){
+        return editText.getText().toString();
+    }
+
+    private void setData(){
+
+        classify = setString(editText_classify);
+        category = setString(editText_category);
+        period = setString(editText_period);
+        frequency = setString(editText_frequency);
+        memberCnt = setString(editText_memberCnt);
+        memberCdt = setString(editText_memberCdt);
+        title = setString(editText_title);
+        description = setString(editText_description);
+
+        String key = createKey();
+        Post post = new Post(key, classify, category, period, frequency, memberCnt, memberCdt, title, description);
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+
+        reference.child("Posts").child(key).setValue(post);
+
+        uploadThumbnail(key);
+    }
+
+    private String createKey() { //이메일 인증코드 생성
+        String[] str = {"A", "B", "C", "D", "E","F","G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S"
+                , "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",
+                "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "-", "_"};
+        String newCode = new String();
+
+        for (int x = 0; x < 20; x++) {
+            int random = (int) (Math.random() * str.length);
+            newCode += str[random];
+        }
+
+        return newCode;
+    }
+
+    private void uploadThumbnail(String key){
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        StorageReference storageRef = storage.getReference();
+        StorageReference postImageRef = storageRef.child("post/" + key);
+
+        // Get the data from an ImageView as bytes
+        imageView_thumbnail.setDrawingCacheEnabled(true);
+        imageView_thumbnail.buildDrawingCache();
+        Bitmap bitmap = ((BitmapDrawable) imageView_thumbnail.getDrawable()).getBitmap();
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] data = baos.toByteArray();
+
+        UploadTask uploadTask = postImageRef.putBytes(data);
+        uploadTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Handle unsuccessful uploads
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                // taskSnapshot.getMetadata() contains file metadata such as size, content-type, etc.
+                // ...
+            }
+        });
+    }
+
+    private void setOnclick(){
+        button_upload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setData();
+            }
+        });
+    }
+}
