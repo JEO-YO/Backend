6af9111be06c08cc1e0e690408b583af54739c13
YeonJun Jo <micayel1219@gmail.com>
2022-01-21 13:44:45 +0900

    14th commit : creating key edit

--- UnitTest/FirebasePost/app/src/main/java/com/unittest/firebasepost/UploadActivity.java
+++ UnitTest/FirebasePost/app/src/main/java/com/unittest/firebasepost/UploadActivity.java
@@ -66,49 +66,61 @@ public class UploadActivity extends AppCompatActivity {
         title = setString(editText_title);
         description = setString(editText_description);
 
-        String key = createKey();
+//        String key = createKey();
+        upload();
+//        checkDuplicatedKey(key, post);
+    }
+
+    private void upload(){
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+        String key = reference.child("Posts").push().getKey();
         Post post = new Post(key, classify, category, period, frequency, memberCnt, memberCdt, title, description,"Posts/"+key);
 
-        checkDuplicatedKey(key, post);
+        reference.child("Posts").child(key).setValue(post);// upload to db
+
+        uploadThumbnail(key);
+        Toast.makeText(UploadActivity.this, "업로드 완료", Toast.LENGTH_SHORT).show();
     }
 
-    private void checkDuplicatedKey(String key, Post post){
-        DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+//    private void checkDuplicatedKey(String key, Post post){
+//        DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+//
+//        reference.child("Posts").orderByKey().equalTo(key).addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                if(snapshot.exists()) {
+//                    //Key exists
+//                    Toast.makeText(UploadActivity.this, "데이터 저장 오류 다시 시도해주세요.", Toast.LENGTH_SHORT).show();
+//                    Log.d(TAG,"Key is duplicated: "+key);
+//                }else{
+//
+//                    reference.child("Posts").child(key).setValue(post);// upload to db
+//
+//                    uploadThumbnail(key);
+//                    Toast.makeText(UploadActivity.this, "업로드 완료", Toast.LENGTH_SHORT).show();
+//                }
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError error) {
+//                Toast.makeText(UploadActivity.this, "취소됨", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//    }
 
-        reference.child("Posts").orderByKey().equalTo(key).addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                if(snapshot.exists()) {
-                    //Key exists
-                    Toast.makeText(UploadActivity.this, "데이터 저장 오류 다시 시도해주세요.", Toast.LENGTH_SHORT).show();
-                    Log.d(TAG,"Key is duplicated: "+key);
-                }else{
-                    reference.child("Posts").child(key).setValue(post);// upload to db
-                    uploadThumbnail(key);
-                    Toast.makeText(UploadActivity.this, "업로드 완료", Toast.LENGTH_SHORT).show();
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-                Toast.makeText(UploadActivity.this, "취소됨", Toast.LENGTH_SHORT).show();
-            }
-        });
-    }
-
-    private String createKey() { //이메일 인증코드 생성
-        String[] str = {"A", "B", "C", "D", "E","F","G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S"
-                , "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",
-                "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "-", "_"};
-        String newCode = new String();
-
-        for (int x = 0; x < 20; x++) {
-            int random = (int) (Math.random() * str.length);
-            newCode += str[random];
-        }
-
-        return newCode;
-    }
+//    private String createKey() { //이메일 인증코드 생성
+//        String[] str = {"A", "B", "C", "D", "E","F","G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S"
+//                , "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",
+//                "t", "u", "v", "w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "-", "_"};
+//        String newCode = new String();
+//
+//        for (int x = 0; x < 20; x++) {
+//            int random = (int) (Math.random() * str.length);
+//            newCode += str[random];
+//        }
+//
+//        return newCode;
+//    }
 
     private void uploadThumbnail(String key){
         FirebaseStorage storage = FirebaseStorage.getInstance();
