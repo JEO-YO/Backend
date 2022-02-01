6d71171c5d9229c469ed80a481ba4ede33025a0e
Yunkeun <yunx2@naver.com>
2022-02-01 14:41:12 +0900

    18th Commit : Edit save logic, edit each string -> User object

--- UnitTest/UserInfoSave/app/src/main/java/com/unittest/userinfosave/model/user/repository/FirebaseUserRepository.java
+++ UnitTest/UserInfoSave/app/src/main/java/com/unittest/userinfosave/model/user/repository/FirebaseUserRepository.java
@@ -18,17 +18,13 @@ public class FirebaseUserRepository implements UserRepository {
     private static final String NAME = "name";
     private final FirebaseDatabase database = FirebaseDatabase.getInstance();
     private final DatabaseReference reference = database.getReference();
-    private final java.lang.String key = reference.child(TABLE).push().getKey();
+    private final String key = reference.child(TABLE).push().getKey(); // UID로 대체
 
     @Override
     public void save(User user) {
+        assert key != null;
         DatabaseReference basePath = reference.child(TABLE).child(key);
-        basePath.child("id").setValue(user.getId());
-        basePath.child("name").setValue(user.getName());
-        basePath.child("university").setValue(user.getUniversity());
-        basePath.child("department").setValue(user.getDepartment());
-        basePath.child("activityArea").setValue(user.getActivityArea());
-        basePath.child("interesting").setValue(user.getInteresting());
+        basePath.setValue(user);
     }
 
     @Override
