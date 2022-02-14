abe3b42aa3041f3d1932d12f513cefdc0fd1ba52
YeonJun Cho <micayel1219@gmail.com>
2022-02-14 21:44:45 +0900

    27th commit : Login add

---
+++ UnitTest/FirebaseReview/app/src/main/java/com/unittest/firebasereview/ReviewActivity.java
@@ -0,0 +1,14 @@
+package com.unittest.firebasereview;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ReviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_review);
+    }
+}
