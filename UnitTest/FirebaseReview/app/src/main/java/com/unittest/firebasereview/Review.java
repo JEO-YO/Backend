89d7eaf079c7a23858c4d63eefa708664ade83fe
YeonJun Cho <micayel1219@gmail.com>
2022-02-15 17:45:25 +0900

    29th commit : Review class set field to list, edit

--- UnitTest/FirebaseReview/app/src/main/java/com/unittest/firebasereview/Review.java
+++ UnitTest/FirebaseReview/app/src/main/java/com/unittest/firebasereview/Review.java
@@ -1,60 +1,25 @@
 package com.unittest.firebasereview;
 
+import java.util.List;
+
 public class Review {
-    String Q1;
-    String Q2;
-    String Q3;
-    String Q4;
-    String Q5;
+    List<String> answers;
 
-    public Review() {
+
+    public Review(){
     }
 
-    public Review(String q1, String q2, String q3, String q4, String q5) {
-        Q1 = q1;
-        Q2 = q2;
-        Q3 = q3;
-        Q4 = q4;
-        Q5 = q5;
+    public Review(List<String> answers) {
+        this.answers = answers;
+
     }
 
-    public String getQ1() {
-        return Q1;
+    public List<String> getAnswers() {
+        return answers;
     }
 
-    public void setQ1(String q1) {
-        Q1 = q1;
+    public void setAnswers(List<String> answers) {
+        this.answers = answers;
     }
 
-    public String getQ2() {
-        return Q2;
-    }
-
-    public void setQ2(String q2) {
-        Q2 = q2;
-    }
-
-    public String getQ3() {
-        return Q3;
-    }
-
-    public void setQ3(String q3) {
-        Q3 = q3;
-    }
-
-    public String getQ4() {
-        return Q4;
-    }
-
-    public void setQ4(String q4) {
-        Q4 = q4;
-    }
-
-    public String getQ5() {
-        return Q5;
-    }
-
-    public void setQ5(String q5) {
-        Q5 = q5;
-    }
 }
