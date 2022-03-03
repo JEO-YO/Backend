10d5b03cdebb8892b027d8cd7ee86b35e7100e94
YeonJun Cho <micayel1219@gmail.com>
2022-03-03 21:48:42 +0900

    37th commit : SetContent Layout miss edit

--- UnitTest/FirebaseReview/app/src/main/java/com/unittest/firebasereview/RetrieveActivity.java
+++ UnitTest/FirebaseReview/app/src/main/java/com/unittest/firebasereview/RetrieveActivity.java
@@ -24,7 +24,7 @@ public class RetrieveActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_review);
+        setContentView(R.layout.activity_retrieve);
         retrieveData();
     }
 
